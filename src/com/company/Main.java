7e7357f78582378bf9e73e7d7555145ba339f1f8
Iaroslav Sviatkin <iaroslav.sviatkin@globallogic.com>
2018-11-08 14:50:25 +0200

    commit6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("sos");
         System.out.println("sos");
-        System.out.println("sos");
-        System.out.println("sos");
+        System.out.println("hi");
+        System.out.println("hi");
 
     }
 }
